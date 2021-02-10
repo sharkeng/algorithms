15e93a3df977119e8ba0a6b9ef6be8096f8e3665
wityat <ttrttr449@gmail.com>
2021-02-10 19:27:32 +0300

    some stuff

--- VNS/VNSearch.py
+++ VNS/VNSearch.py
@@ -1,22 +1,39 @@
-def shaking(factory, iter,max_claster):
+def shaking(factory, count_mashine, count_part,  iter, max_count_clasters):
     pass
-
-
-
-
 
 
 def get_machines_and_parts_from_file(path):
     with open(path) as f:
-        numbers_machine, numbers_part = [int(num) for num in f.readline().split()]
+        count_machine, count_part = [int(num) for num in f.readline().split()]
         factory = list()
-        for i in range(numbers_machine):
-            factory.append([int(num) for num in f.readline().split()][1:])
-    return factory, numbers_machine, numbers_part
+        for i in range(count_machine):
+            factory.append([int(num)-1 for num in f.readline().split()][1:])
+    return factory, count_machine, count_part
 
 
-factory, numbers_machine, numbers_part = get_machines_and_parts_from_file(
-    "dataset/#1 - King and Nakornchai(1982)[Figure-1a] ")
-max_claster=min(numbers_machine,numbers_part)
-shaking(factory,2,max_claster)
+def make_clusters(factory, count_m, count_p, count_clusters):
+
+# 2  - max_count_clusters
+
+if __name__ == "__main__":
+    factory, count_machine, count_part = get_machines_and_parts_from_file(
+        "dataset/#1 - King and Nakornchai(1982)[Figure-1a] ")
+    max_count_clasters = min(count_machine, count_part)
+    shaking(factory, count_machine, count_part, 2, max_count_clasters)
+
+    print(factory)
+# [[2, 4, 5, 6], [1, 3], [1, 3, 7], [2, 4, 6], [1, 7]]
+#
+# [{1:[2]}, {2:[1,3]}]
+# [{3:[7]}, {4:[4, 6]}, {5:[7]}]
+#
+# [[2,4], [1]]
+# [[3,7], [6], [7]]
+#
+#     1   2   3   4   5   6   7
+# 1       #       #   #   #
+# 2   #       #
+# 3   #       #               #
+# 4       #       #       #
+# 5   #                       #
 
